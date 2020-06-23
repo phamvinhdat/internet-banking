b5de336ac12f59defba3c711c244e29860b2a345
datpv <datpv@ghn.vn>
2020-06-23 23:56:41 +0700

    feat: dashboard layout

--- frontend/src/view/dashboard/index.js
+++ frontend/src/view/dashboard/index.js
@@ -1,63 +1,80 @@
 import Header from "./header";
-import React from 'react'
-import {Avatar, Layout, Menu} from "antd";
+import React, {useState} from 'react'
+import {Layout, Menu} from "antd";
 import {
-    AppstoreOutlined,
-    BarChartOutlined,
-    CloudOutlined,
-    ShopOutlined,
-    TeamOutlined,
-    UserOutlined,
-    UploadOutlined,
-    VideoCameraOutlined,
+    WalletOutlined
 } from '@ant-design/icons';
 
 const {Content, Footer, Sider} = Layout;
 
+const WALLET_ITEM = 'wallet'
+const ACCOUNT_ITEM = 'account'
+
 const Dashboard = props => {
 
+    const [content, setContent] = useState('132')
+    const onMenuSelect = obj => {
+        const {key} = obj
+        switch (key) {
+            case WALLET_ITEM:
+                setContent(132)
+                break
+            case  ACCOUNT_ITEM:
+                setContent(125)
+        }
+    }
+
     return (
-        <Layout>
-            <Header/>
+        <Layout
+            style={{
+                height: '100vh',
+                background: 'rgba(255, 255, 255, 0.2)'
+            }}>
+            <Sider
+                collapsible>
+                <Menu theme='dark'
+                      onSelect={onMenuSelect}
+                      mode='inline'
+                      defaultSelectedKeys={[ACCOUNT_ITEM]}>
+                    <div style={{height: '63px'}}/>
+                    <Menu.Divider/>
+                    <Menu.Item
+                        key={ACCOUNT_ITEM}
+                        icon={<WalletOutlined/>}
+                        style={{margin: 1}}>
+                        Thông tin tài khoản
+                    </Menu.Item>
+                    <Menu.Divider/>
+                    <Menu.Item
+                        key={WALLET_ITEM}
+                        icon={<WalletOutlined/>}
+                        style={{margin: 1}}>
+                        Thông tin tài khoản
+                    </Menu.Item>
+                    <Menu.Divider/>
+                </Menu>
+            </Sider>
             <Layout>
-                <Sider
+                <Layout.Header
+                    className='text-center'
                     style={{
-                        overflow: 'auto',
-                        height: '100vh',
-                        position: 'fixed',
-                        left: 0,
-                    }}
-                >
-                    <Menu
-                        theme="dark"
-                        mode="inline"
-                        defaultSelectedKeys={['4']}>
-                        <Menu.Item key="1" icon={<UserOutlined/>}>
-                            Tài khoản
-                        </Menu.Item>
-                        <Menu.Item key="2" icon={<VideoCameraOutlined/>}>
-                            nav 2
-                        </Menu.Item>
-                        <Menu.Item key="3" icon={<UploadOutlined/>}>
-                            nav 3
-                        </Menu.Item>
-                        <Menu.Item key="4" icon={<BarChartOutlined/>}>
-                            nav 4
-                        </Menu.Item>
-                        <Menu.Item key="5" icon={<CloudOutlined/>}>
-                            nav 5
-                        </Menu.Item>
-                        <Menu.Item key="6" icon={<AppstoreOutlined/>}>
-                            nav 6
-                        </Menu.Item>
-                        <Menu.Item key="7" icon={<TeamOutlined/>}>
-                            nav 7
-                        </Menu.Item>
-                        <Menu.Item key="8" icon={<ShopOutlined/>}>
-                            nav 8
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
+                        color: '#fff',
+                        fontSize: 20,
+                        fontFamily: 'Bungee Shade',
+                    }}>
+                    Yasuo bank
+                </Layout.Header>
+                <Content
+                    style={{
+                        margin: '24px 16px 0',
+                        backgroundColor: '#fff'
+                    }}>
+                    {content}
+                </Content>
+                <Footer
+                    className='text-center'>
+                    Yasuo Bank ©2020 Created by 1612091 - 1612234
+                </Footer>
             </Layout>
         </Layout>
     )
