5378c8f43c59f1c6b771c0c095621c9d284d3caf
datpv <datpv@ghn.vn>
2020-07-26 20:43:18 +0700

    fix: get api fe

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const httpSttCode = require('http-status-codes')
 const app = express()
 
 app.use(express.static(path.join(__dirname, '../../frontend/build')))
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
 });
 
