c25503fe6c55e538121903e66955946844131db7
datpv <datpv@ghn.vn>
2020-07-26 16:54:36 +0700

    deploy: add db url

--- backend/config.js
+++ backend/config.js
@@ -16,6 +16,9 @@ const prod = {
         SECRET_KEY: "The quick brown fox jumps over the lazy dog",
         EXPIRATION_TIME: '1h'
     },
+    DATABASE: {
+        URL: 'mysql://dk1rdru82t8fvxue:u2o7k2o3v2mrx2p9@zpj83vpaccjer3ah.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/xcr30w5a4ngim28p'
+    }
 }
 
 const config = process.env.ENV === 'production' ? prod : dev
